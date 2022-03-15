e33c61c2f63f81ab701696172231d99acf5b71d9
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-03-16 01:09:39 +0330

    reservation functionality implemented
    - counter should be implemented
    - pop-up styles need to be added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,57 @@
 import './style.css';
-import reservationEnable from './modules/reservation.js';
+import { getMovies, countMovies } from './modules/get-api.js';
+import { getLikes, addLike } from './modules/interact.js';
+import Reservationspopup from './modules/reservation.js';
 
-reservationEnable();
+const displayMovie = (movie, like = {}) => `<div class="card">
+                    <div class="card-div">
+                        <img class="card-img" src="${movie.image.medium}">
+                    </div>
+                    <div class="description">
+                        <h3 class="title"> ${movie.name} </h3>
+                        <p class="likes"> <i class="fas fa-heart like" data-id="${movie.id}"> <span class="num"> ${like} </span> </i>  likes </p>
+                    </div>
+                    <button class="btn" data-id="${movie.id}"> comments </button>
+                    <button class="reservation btn" data-id="${movie.id}"> reservations </button>
+   </div>`;
+
+const listMovie = document.querySelector('.movies');
+const moviesComponent = async () => {
+  const likes = await getLikes();
+  listMovie.innerHTML = '';
+  const list = await getMovies();
+  countMovies(list);
+  list.forEach((item) => {
+    let count = 0;
+    const num = likes.find((like) => item.id === Number(like.item_id));
+    if (num !== undefined) {
+      count = num.likes;
+    } else {
+      count = 0;
+    }
+    listMovie.innerHTML += displayMovie(item, count);
+  });
+
+  const like = document.querySelectorAll('.like');
+  like.forEach((item) => {
+    item.addEventListener('click', () => {
+      const movieId = item.getAttribute('data-id');
+      if (item.style.color !== 'pink') {
+        item.style.color = 'pink';
+        item.firstElementChild.innerHTML =
+          Number(item.firstElementChild.innerHTML) + 1;
+        addLike(movieId);
+      }
+    });
+  });
+
+  const reservationButtons = document.querySelectorAll('.reservation');
+  reservationButtons.forEach((item) => {
+    item.addEventListener('click', () => {
+      const movieId = item.getAttribute('data-id');
+      Reservationspopup(movieId);
+    });
+  });
+};
+
+moviesComponent();
--- src/modules/reservation.js
+++ src/modules/reservation.js
@@ -1,48 +1,38 @@
-// reservationButtons.setAttribute('movieID', raw.item_id);
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const movieURL = 'https://api.tvmaze.com/shows';
 const reservationsURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/3x4brqQTuutEhv5burqz/reservations/';
 // id: 3x4brqQTuutEhv5burqz
-const reservationButtons = document.querySelector('.reservation');
 const popUpBox = document.getElementById('popUpBox');
 
 const postReservationsData = async (raw) => {
-  await fetch(reservationsURL, {
-    method: 'post',
+  const response = await fetch(reservationsURL, {
+    method: 'POST',
     headers: {
-      'Content-type': 'application/json; charset= UTF-8',
+      'Content-Type': 'application/json; charset= UTF-8',
     },
-    body: raw,
-    redirect: 'follow',
-  })
-  // .then((response) => response.text())
-  // .then((result) => (result.error
-  //   ? { error: true, info: result }
-  //   : { error: false, info: result }))
-  // .catch((error) => ({ error: true, info: error }))
-  ;
+    body: JSON.stringify(raw),
+  }).then((res) => res.text())
+    .then((data) => (data.error
+      ? { error: true, info: data }
+      : { error: false, info: data }))
+    .catch((error) => ({ error: true, info: error }));
+  return response;
 };
 
-const getReservationsData = async (itemID) => {
-  const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/3x4brqQTuutEhv5burqz/reservations/?item_id=${itemID}`, {
-    method: 'GET',
-    redirect: 'follow',
-  })
-  // .then((response) => response.json())
-  // .then((result) => result)
-  // .catch((error) => error);
-  ;
-  const reservationsList = await response.json();
-  return reservationsList;
+const getReservationsData = async (movieId) => {
+  const response = await fetch(`${reservationsURL}?item_id=${movieId}`).catch((err) => err);
+  return response.json();
 };
 
-const reservationDisplay = (movieID) => {
-  getReservationsData(movieID).then((data) => {
+const reservationsDisplay = (movieId) => {
+  popUpBox.querySelector('.reservationTable').innerHTML = '';
+  getReservationsData(movieId).then((data) => {
     if (!data.error) {
-      data.forEach((reserve) => {
-        popUpBox.querySelector('.reservationTable').innerHTML += `<li class="reserv-li"> ${reserve.date_start} - ${reserve.date_end} by ${reserve.username} </li>`;
+      data.forEach((comment) => {
+        popUpBox.querySelector('.reservationTable').innerHTML += `<li class="reservation-li"> ${comment.date_start} - ${comment.date_end} by ${comment.username}</li>`;
       });
     } else {
-      popUpBox.querySelector('.reservationTable').innerHTML ='There is no reservation yet for this movie!';
+      popUpBox.querySelector('.reservationTable').innerHTML = 'no reservation yet!';
     }
   });
 };
@@ -51,54 +41,70 @@ const getMovieData = async (movieID) => {
   const response = await fetch(`${movieURL}/${movieID}`, {
     method: 'GET',
     redirect: 'follow',
-  });
-  return response.json();
+  })
+    .then((response) => response.json())
+    .then((result) => result)
+    .catch((error) => error);
+  return response;
 };
 
 const Reservationspopup = (movieID) => {
-  // const popup = document.createElement('div');
-  // popup.classList.add('modal');
-  popUpBox.innerHTML = `loading...<br>
-  <span class="closeReservation">X</span>`;
   getMovieData(movieID).then((result) => {
-    popUpBox.innerHTML = `<span class="closeReservation">X</span>
+    popUpBox.innerHTML = `
+    <span class="closeReservation"><img src="https://img.icons8.com/doodle/48/000000/delete-sign.png"/></span>
+    <div class="reservation-skeleton">
+    <div class="reservation-film-info">
     <h2 class="popupTitle">${result.name}</h2>
-    <img src="${result.image.medium}" class="popupIMG">
-    <h3 class="reservationsTitle">Comments (<span class="reservationCounter">0</span>)</h3>
+    <img src="${result.image.medium}" class="reservationImg">
+    </div>
+    <div>
+    <ul>
+      <li>Premiered: ${result.premiered}</li>
+      <li>Ended: ${result.ended}</li>
+      <li>Language: ${result.language}</li>
+      <li>Type: ${result.type}</li>
+    </ul>
+    </div>
+    <div class="reservation-info">
+    <h3 class="reservationsTitle"> Reservations (<span class="reservationCounter">0</span>)</h3>
     <ul class="reservationTable"></ul>
     <form class="reservationForm">
       <h3>Add a reservation</h3>
-      <input type="text" name="name" placeholder="Your name" required>
-      <input type="date" name="StartDate" placeholder="Start date" required>
-      <input type="date" name="EndDate" placeholder="End date" required>
+      <input id="username" type="text" name="username" placeholder="Your name" required>
+      <br>
+      <input id="StartDate" type="date" name="StartDate" placeholder="Start date" required>
+      <br>
+      <input id="EndDate" type="date" name="EndDate" placeholder="End date" required>
+      <br>
       <button class="reservationSubmit" type="submit">Submit</button>
     </form>
+    </div>
+    </div>
     `;
-    reservationDisplay(movieID);
+    popUpBox.style.display = 'flex';
+    document.querySelector('.closeReservation').addEventListener('click', () => {
+      popUpBox.style.display = 'none';
+      popUpBox.innerHTML = '';
+    });
+    reservationsDisplay(movieID);
     const form = popUpBox.querySelector('.reservationForm');
     form.addEventListener('submit', (event) => {
       event.preventDefault();
-      const raw = JSON.stringify({
+      const user = form.elements.username.value;
+      const StartDate = form.elements.StartDate.value;
+      const EndDate = form.elements.EndDate.value;
+      postReservationsData({
         item_id: movieID,
-        username: form.elements.username.value,
-        date_start: form.elements.StartDate.value,
-        date_end: form.elements.EndDate.value,
+        username: user,
+        date_start: StartDate,
+        date_end: EndDate,
+      }).then(() => {
+        reservationsDisplay(movieID);
+        // counter++
+        form.reset();
       });
-      postReservationsData(raw);
-      form.reset();
-    });
-    popUpBox.style.display = 'flex';
-  });
-};
-
-const reservationEnable = () => {
-  reservationButtons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      const movieID = btn.getAttribute('movieID');
-      Reservationspopup(movieID);
-      // document.body.style.overflow = 'hidden';
     });
   });
 };
 
-export default reservationEnable;
+export default Reservationspopup;
