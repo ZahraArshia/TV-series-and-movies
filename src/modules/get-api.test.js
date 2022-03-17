30c9b3ae299e907b77bffdf4a75d7f5c6072f459
KABIKA681 <pascalkabikampan@gmail.com>
2022-03-17 23:49:34 +0200

    Add test to the app

---
+++ src/modules/get-api.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { countMovies } from './get-api';
+
+test('displays number of items on home-page', () => {
+  document.body.innerHTML = '<h1 class="count-movies"></h1>';
+  countMovies([1, 2, 3, 4, 5, 6, 7, 59, 89, 20]);
+  expect(document.querySelector('.count-movies').innerHTML).toBe('10');
+});
+
+test('displays nothing if there are no items on home-page', () => {
+  document.body.innerHTML = '<h1 class="count-movies"></h1>';
+  countMovies([]);
+  expect(document.querySelector('.count-movies').innerHTML).toBe('');
+});
